e483288906dd8ded2b647a3cf9e76e5a72ddb27c
Yago Silva <yagosilvager@gmail.com>
2024-07-09 13:06:08 -0300

    first commit

---
+++ src/main/java/onde/digitalinnovation/gof/service/ViaCepService.java
@@ -0,0 +1,13 @@
+package onde.digitalinnovation.gof.service;
+
+import onde.digitalinnovation.gof.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
